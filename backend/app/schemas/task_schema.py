d04848c795c33a7a2fdb924bec4ab30ae38f8fab
Ankit Chaudhary <ankit.chaudhary@infoorigin.com>
2026-03-11 11:57:20 +0530

    schema created

---
+++ backend/app/schemas/task_schema.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+class TaskCreate(BaseModel):
+    title:str
+    description:str
+
+class TaskUpdate(BaseModel):
+    title:str
+    description:str
+    completed:bool
+
+class TaskResponse(BaseModel):
+    id:int
+    title:str
+    description:str
+    completed:bool
+
+    class Config:
+        orm_mode=True
